73cae7f7c5e2f3872bf086d013ad8d15866412b2
bmartin <b.martin@shinken-solutions.com>
2025-05-16 16:31:43 +0200

    SEF-11948 - 003.7.4.3 - fix for py 3.11 ( encode in guess_server_const_uuid )

--- opsbro/util.py
+++ opsbro/util.py
@@ -183,9 +183,9 @@ def guess_server_const_uuid():
             openvz_id = int(buf.strip().split(' ')[0])
             servr_uniq_id = '%s-%d' % (socket.gethostname().lower(), openvz_id)
             logger.info('[SERVER-UUID] OpenVZ: using the hostname & openvz local id as server unique UUID: %s' % servr_uniq_id)
-            return hashlib.sha1(servr_uniq_id).hexdigest()
+            return hashlib.sha1(unicode_to_bytes(servr_uniq_id)).hexdigest()
     # No merly fixed stuff? ok, pure randomness
-    return hashlib.sha1(get_uuid()).hexdigest()
+    return hashlib.sha1(unicode_to_bytes(get_uuid())).hexdigest()
 
 
 # recursivly change a dict with pure bytes
